9fa43276574639df09cf3ce41781f807ad0973c7
viktorgajdosik <v.g.gajdosik@gmail.com>
2024-09-18 03:09:36 +0200

    g

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,22 +1,27 @@
 <div class="listing-card p-3">
     <div class="date-container">
-        <p class="date-created d-none d-md-block"> <small>
-            <i class="fa fa-calendar" data-toggle="tooltip" title="Date Created"></i> {{ $listing->created_at->format('d/m/Y') }}</small>
+        <p class="date-created d-none d-md-block">
+            <small>
+                <i class="fa fa-calendar" data-toggle="tooltip" title="Date Created"></i> {{ $listing->created_at->format('d/m/Y') }}
+            </small>
         </p>
     </div>
     <h5 class="listing-title mb-2">{{ $listing->title }}</h5>
-        <span><small>
-            <i class="fa fa-user" data-toggle="tooltip" title="Author"></i> {{ $listing->author }}</small>
-        </span>
-        <span> | </span>
-        <span><small>
-            <i class="fa solid fa-building" data-toggle="tooltip" title="Department"></i> {{ $listing->department }}</small>
-        </span>
-        <span class="d-inline d-md-none"> | </span>
-        <span class="d-inline d-md-none"><small>
-            <i class="fa fa-calendar" data-toggle="tooltip" title="Date Created"></i> {{ $listing->created_at->format('d/m/Y') }}</small>
-        </span>
-    <p class="text-secondary description mt-2 mb-0 text-justify">{{ Str::limit($listing->description, 200) }} <span class="expand-listing text-info d-none d-md-inline end-0" data-toggle="modal" data-target="#descriptionModal" data-title="{{ $listing->title }}" data-description="{{ $listing->description }}"> Preview</span></p>
+    <span><small>
+        <i class="fa fa-user" data-toggle="tooltip" title="Author"></i> {{ $listing->author }}</small>
+    </span>
+    <span> | </span>
+    <span><small>
+        <i class="fa solid fa-building" data-toggle="tooltip" title="Department"></i> {{ $listing->department }}</small>
+    </span>
+    <span class="d-inline d-md-none"> | </span>
+    <span class="d-inline d-md-none"><small>
+        <i class="fa fa-calendar" data-toggle="tooltip" title="Date Created"></i> {{ $listing->created_at->format('d/m/Y') }}</small>
+    </span>
+    <p class="text-secondary description mt-2 mb-0 text-justify">
+        {{ Str::limit($listing->description, 200) }}
+        <span class="expand-listing text-info d-none d-md-inline end-0 preview-link" data-toggle="modal" data-target="#descriptionModal" data-title="{{ $listing->title }}" data-description="{{ $listing->description }}"> Preview</span>
+    </p>
 
     <div class="modal fade" id="descriptionModal" tabindex="-1" role="dialog" aria-labelledby="descriptionModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,58 +1,67 @@
 <x-head>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="card login-card">
-                <div class="card-header border-bottom-0" style="background-color: transparent; text-align: center;">
-                    <div class="mb-2 mt-2" style="background-color: #007BFF; width: 50px; height: 50px; border-radius: 50%; margin: 0 auto;">
-                        <i class="fa fa-user fa-2x" style="color: white; padding: 8px;"></i>
+    <div class="container-fluid vh-100">
+        <div class="row h-100">
+            <!-- Left Column - Full Blue with Centered Logo, Header, and Text -->
+            <div class="col-xl-5 d-none d-xl-flex flex-column align-items-center justify-content-center bg-primary text-white">
+                <div class="text-wrapper" style="max-width: 400px; text-align: left;">
+                    <h2 class="font-weight-bold">Collaborative research</h2>
+                    <p>
+                        Join the effort to advance medical research through collaboration. Together, we can drive innovation and improve healthcare outcomes globally.
+                    </p>
+                </div>
+                </p>
+            </div>
+
+            <!-- Right Column - Transparent Login Card -->
+            <div class="col-xl-7 d-flex align-items-center justify-content-center">
+                <div class="card login-card" style="background-color: transparent; border: none;">
+                    <div class="card-header border-bottom-0 text-center" style="background-color: transparent;">
+                        <h2 class="font-weight-bold" style="color: #000000;">Welcome</h2>
+                        <p class="login-subtitle">Sign in to access your account.</p>
                     </div>
-                    <h3 style="color: black;">Welcome</h3>
-                    <p class="login-subtitle">Sign in to access your account.</p>
-                </div>
-                <div class="card-body">
-                    <form method="POST" action="/users/authenticate">
-                        @csrf
-                        <div class="form-group">
-                            <div class="input-group">
-                                <div class="input-group-prepend" style="width: 40px;">
-                                    <span class="input-group-text bg-light border-0" style="width: 80px; justify-content: center; border-radius:7px 0px 0px 7px;">
-                                        <i class="fa fa-envelope"></i>
-                                    </span>
+                    <div class="card-body">
+                        <form method="POST" action="/users/authenticate">
+                            @csrf
+                            <!-- Email Input -->
+                            <div class="form-group">
+                                <div class="input-group-lg">
+                                    <input type="email" class="form-control custom-input" id="email" name="email" placeholder="Enter email" required value="{{ old('email') }}">
                                 </div>
-                                <input type="email" class="form-control bg-light border-0" id="email" name="email" placeholder="Enter email" required value="{{ old('email') }}">
+                                @error('email')
+                                <p class="text-danger mt-1">{{ $message }}</p>
+                                @enderror
                             </div>
-                            @error('email')
-                            <p class="text-danger mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <div class="input-group">
-                                <div class="input-group-prepend" style="width: 40px;">
-                                    <span class="input-group-text bg-light border-0" style="width: 80px; justify-content: center; border-radius:7px 0px 0px 7px;">
-                                        <i class="fa fa-lock"></i>
-                                    </span>
-                                </div>
-                                <input type="password" class="form-control bg-light border-0" id="password" name="password" placeholder="Password" required>
-                                <div class="input-group-append">
-                                    <span class="input-group-text bg-light border-0" style="cursor: pointer; background-color: white; border-radius:0px 7px 7px 0px;" onclick="togglePasswordVisibility()">
-                                        <i class="fa fa-eye"></i>
-                                    </span>
-                                </div>
+
+                           <!-- Password Input -->
+<div class="form-group">
+    <div class="input-group input-group-lg">
+        <input type="password" class="form-control custom-input-1" id="password" name="password" placeholder="Password" required>
+        <div class="input-group-append">
+            <span class="input-group-text icon-box-1" style="cursor: pointer;" onclick="togglePasswordVisibility()">
+                <i class="fa fa-eye icon"></i>
+            </span>
+        </div>
+    </div>
+    @error('password')
+    <p class="text-danger mt-1">{{ $message }}</p>
+    @enderror
+</div>
+
+
+                            <!-- Sign In Button -->
+                            <div class="mt-3">
+                                <button type="submit" class="btn-lg btn-primary btn-block" id="loginButton">Sign in</button>
                             </div>
-                            @error('password')
-                            <p class="text-danger mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div class="mt-3">
-                            <button type="submit" class="btn btn-primary btn-block" id="loginButton" disabled>Sign in</button>
-                        </div>
-                        <div class="mt-2">
-                            <a href="" class="ml-auto">Forgot Password</a>
-                        </div>
-                    </form>
-                </div>
-                <div class="card-footer text-center border-top-1" style="background-color: transparent;">
-                    Don't have an account? <a href="/register">Sign up</a>
+                            <div class="mt-2 text-center">
+                                <a href="#">Forgot Password</a>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!-- Sign Up Link -->
+                    <div class="card-footer text-center" style="background-color: transparent;">
+                        Don't have an account? <a href="/register">Sign up</a>
+                    </div>
                 </div>
             </div>
         </div>
