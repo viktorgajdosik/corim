cc9690d39667e5272bd4ac4727e3b4368ecb172d
viktorgajdosik <v.g.gajdosik@gmail.com>
2024-09-28 13:25:01 +0200

    D

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,78 +1,70 @@
 <x-head>
-        <div class="sidebar">
+    <div class="sidebar">
+        <div class="list-group list-group-flush">
+            <a href="/listings/manage" class="list-group-item list-group-item-action {{ request()->is('listings/manage*') ? 'active' : '' }}">
+                <i class="fa fa-list sidebar-icon"></i><span>Management</span>
+            </a>
+            <a href="/users/profile" class="list-group-item list-group-item-action {{ request()->is('users/profile*') ? 'active' : '' }}">
+                <i class="fa fa-user sidebar-icon"></i><span>Profile</span>
+            </a>
+            <a href="/listings/create" class="list-group-item list-group-item-action {{ request()->is('listings/create*') ? 'active' : '' }}">
+                <i class="fa fa-plus-circle sidebar-icon"></i><span>Create Listing</span>
+            </a>
+            <a href="" class="list-group-item list-group-item-action">
+                <i class="fa fa-bell sidebar-icon"></i><span>Notifications</span>
+            </a>
+            @auth
+            <form method="POST" action="/logout">
+                @csrf
+                <button type="submit" class="list-group-item list-group-item-action">
+                    <i class="fa fa-sign-out sidebar-icon"></i><span>Log out</span>
+                </button>
+            </form>
+            @else
+            <a href="/login" class="list-group-item list-group-item-action {{ request()->is('login*','register*') ? 'active' : '' }}" style="margin-bottom: 10px">
+                <i class="fa fa-sign-in sidebar-icon"></i><span>Sign in/Sign up</span>
+            </a>
+            @endauth
+        </div>
+    </div>
 
-            <div class="list-group list-group-flush">
-                <a href="/listings/manage" class="list-group-item list-group-item-action {{ request()->is('listings/manage*') ? 'active' : '' }}">
-                    <i class="fa fa-list sidebar-icon"></i><span>Management</span>
-                </a>
-                <a href="/users/profile" class="list-group-item list-group-item-action {{ request()->is('users/profile*') ? 'active' : '' }}">
-                    <i class="fa fa-user sidebar-icon"></i><span>Profile</span>
-                </a>
-                <a href="/listings/create" class="list-group-item list-group-item-action {{ request()->is('listings/create*') ? 'active' : '' }}">
-                    <i class="fa fa-plus-circle sidebar-icon"></i><span>Create Listing</span>
-                </a>
-                <a href="" class="list-group-item list-group-item-action">
-                    <i class="fa fa-bell sidebar-icon"></i><span>Notifications</span>
-                </a>
-                @auth
-                <form method="POST" action="/logout">
+    <div class="fixed-bottom">
+        <ul class="nav justify-content-between navbar-bottom-ul w-100">
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('listings/manage*') ? 'active' : '' }}" href="/listings/manage"><i class="fa fa-list"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('users/profile*') ? 'active' : '' }}" href="/users/profile"><i class="fa fa-user"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('listings/create') ? 'active' : '' }}" href="/listings/create"><i class="fa fa-plus-circle"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=""><i class="fa fa-bell"></i></a>
+            </li>
+            @auth
+            <li class="nav-item">
+                <form method="POST" class="m-0" action="/logout">
                     @csrf
-                    <button type="submit" class="list-group-item list-group-item-action">
-                        <i class="fa fa-sign-out sidebar-icon"></i><span>Log out</span>
+                    <button type="submit" class="nav-link" style="border: none; background: none;">
+                        <i class="fa fa-sign-out"></i>
                     </button>
                 </form>
-                @else
-                <a href="/login" class="list-group-item list-group-item-action {{ request()->is('login*','register*') ? 'active' : '' }}" style="margin-bottom: 10px">
-                    <i class="fa fa-sign-in sidebar-icon"></i><span>Sign in/Sign up</span>
-                </a>
-                @endauth
-            </div>
-
-        </div>
-
-        <div class="fixed-bottom">
-
-              <ul class="navbar-nav navbar-bottom-ul d-flex justify-content-between w-100">
-                <li class="nav-item">
-                <a class="nav-link {{ request()->is('listings/manage*') ? 'active' : '' }}" href="/listings/manage"><i class="fa fa-list"></i></a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link {{ request()->is('users/profile*') ? 'active' : '' }}" href="/users/profile"><i class="fa fa-user"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->is('listings/create') ? 'active' : '' }}" href="/listings/create"><i class="fa fa-plus-circle"></i></a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href=""><i class="fa fa-bell"></i></a>
-                </li>
-                @auth
-                <li class="nav-item">
-                    <form method="POST" class="m-0" action="/logout">
-                        @csrf
-                        <button type="submit" class="nav-link nav-link-out" style="border: none; background: none;">
-                            <i class="fa fa-sign-out"></i>
-                        </button>
-                    </form>
-                   </li>
-                    @else
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('login*','register*') ? 'active' : '' }}" href="/login"><i class="fa fa-sign-in"></i></a>
-                        </li>
-                    @endauth
-
-              </ul>
-
-
-        </div>
-
-<div class="container container-custom-main">
-{{$slot}}
-<footer class="mt-4" style="margin-bottom: 100px;">
-    <div class="container text-center">
-        <p>&copy; <?php echo date('Y'); ?> CORIM - Collaborative Research Initiative in Medicine</p>
+            </li>
+            @else
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('login*','register*') ? 'active' : '' }}" href="/login"><i class="fa fa-sign-in"></i></a>
+            </li>
+            @endauth
+        </ul>
     </div>
-</footer>
-</div>
 
+    <div class="container container-custom-main">
+        {{$slot}}
+        <footer class="mt-4" style="margin-bottom: 100px;">
+            <div class="container text-center">
+                <p>&copy; <?php echo date('Y'); ?> CORIM - Collaborative Research Initiative in Medicine</p>
+            </div>
+        </footer>
+    </div>
 </x-head>
-
--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -39,7 +39,28 @@
                     <option value="" selected disabled>Select a department</option>
                     <option value="Anaesthesiology, Resuscitation and Intensive Care Medicine">Anaesthesiology, Resuscitation and Intensive Care Medicine</option>
                     <option value="Anatomy">Anatomy</option>
-                    <!-- Add other department options here -->
+                    <option value="Clinical Biochemistry">Clinical Biochemistry</option>
+                    <option value="Clinical Neurosciences">Clinical Neurosciences</option>
+                    <option value="Craniofacial Surgery">Craniofacial Surgery</option>
+                    <option value="Dentistry">Dentistry</option>
+                    <option value="Emergency Medicine">Emergency Medicine</option>
+                    <option value="Epidemiology and Public Health">Epidemiology and Public Health</option>
+                    <option value="Forensic Medicine">Forensic Medicine</option>
+                    <option value="Gynecology and Obstetrics">Gynecology and Obstetrics</option>
+                    <option value="Hematooncology">Hematooncology</option>
+                    <option value="Histology and Embryology">Histology and Embryology</option>
+                    <option value="Hyperbaric Medicine">Hyperbaric Medicine</option>
+                    <option value="Imaging Methods">Imaging Methods</option>
+                    <option value="Internal Medicine">Internal Medicine</option>
+                    <option value="Medical Microbiology">Medical Microbiology</option>
+                    <option value="Molecular and Clinical Pathology and Medical Genetics">Molecular and Clinical Pathology and Medical Genetics</option>
+                    <option value="Nursing and Midwifery">Nursing and Midwifery</option>
+                    <option value="Oncology">Oncology</option>
+                    <option value="Pediatrics">Pediatrics</option>
+                    <option value="Pharmacology">Pharmacology</option>
+                    <option value="Physiology and Pathophysiology">Physiology and Pathophysiology</option>
+                    <option value="Rehabilitation and Sports Medicine">Rehabilitation and Sports Medicine</option>
+                    <option value="Surgical Studies">Surgical Studies</option>
                 </select>
                 <label for="department">Department</label>
                 @error('department')
