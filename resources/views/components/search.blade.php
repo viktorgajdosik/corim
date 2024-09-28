5de8d040d5683e44eb6a3563526c8d813ddce5ae
viktorgajdosik <v.g.gajdosik@gmail.com>
2024-09-28 15:29:37 +0200

    s

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -8,15 +8,22 @@
 
     <div class="container container-custom-top">
 
+
         <form class="form-inline w-100 p-0 m-0" action="/" method="get">
             <div class="input-group">
+                <div class="logo_wrapper_m pr-3">
+                    <a href="/">
+                        <img src="{{ asset('images/logo_icon.svg') }}" alt="CORIM Logo" class="logo_m">
+                    </a>
+                </div>
                 <!-- Input takes full width but leaves space for the button -->
                 <input
-                    class="form-control-lg border-0 flex-grow-1"
+                    class="form-control-lg bg-white border-0 flex-grow-1"
                     type="search"
                     placeholder="Search listings"
                     aria-label="Search"
                     name="search"
+                    style="margin-left: 25px; border-radius: 7px"
                 />
 
                 <!-- Search button aligned to the right -->
