7973b0b9b72ae3691e5d8849160cf9a0c85afaea
viktorgajdosik <v.g.gajdosik@gmail.com>
2025-03-25 18:46:17 +0100

    s

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,16 +1,4 @@
-<div>
-    @if ($isPlaceholder ?? false)
-        <!-- Placeholder (Matches Real Listing Size) -->
-        <div class="listing-card p-3 placeholder-glow" style="min-height: 140px; width: 100%;">
-            <h5 class="listing-title mb-2">
-                <span class="placeholder text-secondary col-12"></span>
-            </h5>
-            <p class="text-secondary description mt-2 text-justify">
-                <span class="placeholder text-secondary col-12"></span>
-                <span class="placeholder text-secondary col-10"></span>
-            </p>
-        </div>
-    @else
+
         <!-- Determine if the user is the author -->
         @php
             $isAuthor = auth()->check() && auth()->id() === $listing->user_id;
@@ -20,7 +8,7 @@
         <a href="{{ $isAuthor ? route('listings.show-manage', $listing) : route('listings.show', $listing) }}"
            class="text-decoration-none text-white">
             <div class="listing-card mb-3 p-3">
-                <p class="fw-bolder listing-title mb-2 fs-5">{{ $listing->title }}</p>
+                <p class="fw-bolder listing-title mb-2 fs-6">{{ $listing->title }}</p>
                 <span><small>
                     <i class="fa fa-user"></i> {{ $listing->author }}</small>
                 </span>
@@ -32,11 +20,11 @@
                 <span><small>
                     <i class="fa fa-calendar"></i> {{ $listing->created_at->format('d/m/Y') }}</small>
                 </span>
-                <p class="description mt-2 mb-0 text-justify">
+                <small> <p class="description mt-2 mb-0 text-justify">
                     {{ Str::limit($listing->description, 230) }}
-                </p>
+                </p></small>
+
             </div>
         </a>
-    @endif
-    <hr class="text-white">
-</div>
+
+
