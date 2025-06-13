98b81ace720e88b92c5a89f5b2513af71244801d
viktorgajdosik <v.g.gajdosik@gmail.com>
2025-06-13 23:41:48 +0200

    d

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -3,10 +3,8 @@
 @section('content')
 <div class="row align-items-stretch">
     <!-- Personal Information Section -->
-    <div class="col-xl-3 mb-5">
+    <div class="col-xl-3">
         <x-secondary-heading>Personal Information</x-secondary-heading>
-        <br>
-
         <section
             x-data="{ modalOpen: false, countdown: 5, timer: null, confirming: false }"
             class="relative text-white"
@@ -97,7 +95,7 @@
     </div>
 
     <!-- Research Participation Section -->
-    <div class="col-xl-9 mb-5">
+    <div class="col-xl-9">
         <x-secondary-heading>Research Participation
             <i class="fa fa-info-circle ml-2 info-icon"
                data-bs-toggle="popover"
@@ -106,7 +104,6 @@
                data-bs-content="This section displays your participation in the research work created by other authors. You can see your own research listings by clicking 'Management' in the menu.">
             </i>
         </x-secondary-heading>
-        <br>
         <div class="scrollable-listings border-0 h-75">
             @if ($user->acceptedApplications->isEmpty())
                 <x-text>You have not participated in other author's research work yet.</x-text>
