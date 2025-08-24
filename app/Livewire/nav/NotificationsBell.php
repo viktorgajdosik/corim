9e8c1ce003570e30f6d7cfe2aa44753d9442bd39
viktorgajdosik <v.g.gajdosik@gmail.com>
2025-08-24 16:19:50 +0200

    s

--- app/Livewire/nav/NotificationsBell.php
+++ app/Livewire/nav/NotificationsBell.php
@@ -27,7 +27,7 @@ class NotificationsBell extends Component
 
     public function render()
     {
-        return view('livewire.nav.notifications-bell');
+        return view('\App\Livewire\nav\NotificationsBell');
     }
 }
 
