fa1957d819756f7635bde4bc729e3261adb26642
viktorgajdosik <v.g.gajdosik@gmail.com>
2025-08-24 16:08:20 +0200

    s

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
                 <li class="nav-item"><a class="nav-link {{ request()->is('listings/manage*', 'listings/show-manage*') ? 'active' : '' }}" href="/listings/manage"><i class="fa fa-list"></i></a></li>
                 <li class="nav-item"><a class="nav-link {{ request()->is('users/profile*') ? 'active' : '' }}" href="/users/profile"><i class="fa fa-user"></i></a></li>
                 <li class="nav-item"><a class="nav-link {{ request()->is('listings/create') ? 'active' : '' }}" href="/listings/create"><i class="fa fa-plus-circle"></i></a></li>
-            @livewire(\App\Livewire\Nav\NotificationsBell::class)
+            @livewire(\App\Livewire\nav\NotificationsBell::class)
 
                 @auth
                     <li class="nav-item">
